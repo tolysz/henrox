32051534e6166a1ecbe6af23b3ed018c3e9db11b
Marcin Tolysz <marcin.tolysz@openbet.com>
2015-10-01 17:13:52 +0100

    .

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,17 @@
+{-# LAnguage OverloadedStrings, QuasiQuotes #-}
 module Main where
 
-main = do
-  print "https://m.tenrox.net/default.aspx?v=3.0.1&device=ios&organization=OrgName"
+import Data.String.QM
+import Data.String
 
+mobapi = "https://m.tenrox.net/2014R3/tenterprise/api/"
+org = "OpenBet"
+
+wsdlUrl :: String -> String
+wsdlUrl serv = [qm|https://${org}.tenrox.net/TWebService/${serv}.svc?wsdl|]
+-- https://openbet.tenrox.net/TWebService/Timesheets.svc
+-- loginUrl org user pass =
+main = do
+  org = "OpenBet"
+  putStrLn [qm|https://m.tenrox.net/default.aspx?v=3.0.1&device=ios&organization=${org}|]
+  putStrLn $ wsdlUrl "Timesheets"
